4430bd25d0e3eaab69e1be4c760a0de306adeb06
Mehakmangla2003 <101574131+Mehakmangla2003@users.noreply.github.com>
2023-11-30 14:10:32 +0530

    Add files via upload

---
+++ controller/productController.js
@@ -0,0 +1,97 @@
+const Product = require('../models/products');
+
+// Create a new product
+const createProduct = async (req, res) => {
+  try {
+    const { name, price, description } = req.body;
+    const product = new Product({ name, price, description });
+    const savedProduct = await product.save();
+    res.json(savedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Get all products with search, sort, filter, and pagination
+const getAllProducts = async (req, res) => {
+  try {
+    // Pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    // Sorting
+    const sortField = req.query.sortBy || 'name';
+    const sortOrder = req.query.sortOrder === 'desc' ? -1 : 1;
+    const sortOptions = { [sortField]: sortOrder };
+
+    // Filtering
+    const filterOptions = {};
+    if (req.query.search) {
+      filterOptions.name = { $regex: new RegExp(req.query.search, 'i') };
+    }
+    // You can add more filters as needed
+
+    // Query
+    const products = await Product.find(filterOptions)
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(limit);
+
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get a single product by ID
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Update a product by ID
+const updateProductById = async (req, res) => {
+  try {
+    const { name, price, description } = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.productId,
+      { name, price, description },
+      { new: true } // Return the updated document
+    );
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete a product by ID
+const deleteProductById = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.productId);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(deletedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+};
---
+++ routes/product.js
@@ -0,0 +1,22 @@
+// routes/productRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const productController = require('../controller/productController');
+
+// Create a new product
+router.post('/', productController.createProduct);
+
+// Get all products
+router.get('/', productController.getAllProducts);
+
+// Get a single product by ID
+router.get('/:productId', productController.getProductById);
+
+// Update a product by ID
+router.put('/:productId', productController.updateProductById);
+
+// Delete a product by ID
+router.delete('/:productId', productController.deleteProductById);
+
+module.exports = router;
