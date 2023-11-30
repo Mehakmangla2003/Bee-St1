0e91ee862e773404e1f16a196317c5b6c8d27128
Mehakmangla2003 <101574131+Mehakmangla2003@users.noreply.github.com>
2023-11-30 14:39:15 +0530

    app.js

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 const productRoutes = require('./routes/product');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 
 
-// Middleware
+
 app.use(express.json());
 
-// Routes
+
 app.use('/products', productRoutes);
 
 mongoose.connect("mongodb+srv://mehak0877be21:Mehak062001@cluster0.jajmxwz.mongodb.net/?retryWrites=true&w=majority"
